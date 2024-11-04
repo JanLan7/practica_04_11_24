b7578443141f3e16b0b603b7a9f8c053fb9b6fed
JanLan <idiomasjanlan@gmail.com>
2024-11-04 20:07:03 -0300

    practica

---
+++ main4.js
@@ -0,0 +1,14 @@
+let booleanos = [false, true, false, true]
+
+console.log(booleanos);
+
+
+let matriz = [
+    ["jazz", "pop", "tecno"],
+    [1,2,3],
+    [true,false,true]
+]
+
+console.log(matriz);
+console.log(matriz[0]);
+console.log(matriz[0][2]);
